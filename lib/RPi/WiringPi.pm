0f8988d2b3932a6a4f4cd1b4e8222d149f2be6f7
Steve Bertrand <steveb@cpan.org>
2017-02-02 16:34:13 -0700

    bumped to 2.36.8

--- lib/RPi/WiringPi.pm
+++ lib/RPi/WiringPi.pm
@@ -14,7 +14,7 @@ use RPi::WiringPi::Interrupt;
 use RPi::WiringPi::LCD;
 use RPi::WiringPi::Pin;
 
-our $VERSION = '2.36.7';
+our $VERSION = '2.36.8';
 
 my $fatal_exit = 1;
 
--- lib/RPi/WiringPi/LCD.pm
+++ lib/RPi/WiringPi/LCD.pm
@@ -3,7 +3,7 @@ package RPi::WiringPi::LCD;
 use strict;
 use warnings;
 
-our $VERSION = '2.36.7';
+our $VERSION = '2.36.8';
 
 use parent 'WiringPi::API';
 use RPi::WiringPi::Constant qw(:all);
