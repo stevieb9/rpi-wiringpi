4d6da5ef6063c04c81469a4b5f2690b36ba21c93
Steve Bertrand <steveb@cpan.org>
2018-05-19 11:44:51 -0700

    more work on rtc.c

--- test/rtc.c
+++ test/rtc.c
@@ -10,15 +10,23 @@
 #define RTC_SEC     0x00
 #define RTC_MIN     0x01
 #define RTC_HOUR    0x02
-#define RTC_DAY     0x03    // day of week (1-7)
-#define RTC_DATE    0x04 // day of month (1-31)
+#define RTC_DAY     0x03 // day of week (1-7)
+#define RTC_MDAY    0x04 // day of month (1-31)
 #define RTC_MONTH   0x05
 #define RTC_YEAR    0x06
 
 #define RTC_AM_PM   0x20
 #define RTC_12_24   0x40
 
-int deviceI2CAddress = 0x68;
+struct dateTime {
+    uint8_t sec;
+    uint8_t min;
+    uint8_t hour;
+    uint8_t day;
+    uint8_t mday;
+    uint8_t month;
+    uint8_t year;
+};
 
 int dec2bcd(int num){
    return((num/10) * 16 + (num%10));
@@ -31,6 +39,10 @@ int setRegister(int fd, int reg, uint8_t value, char* name){
         return -1;
     }
     return 0;
+}
+
+int getRegister(int want, int dt[7]){
+    return dt[want];
 }
 
 int main (void)
@@ -53,44 +65,38 @@ int main (void)
         printf("Couldn't find device at addr %d: %s\n", RTC_ADDR, strerror(errno));
 		return -1;
 	}  
-  
+/* 
     setRegister(deviceHandle, RTC_SEC, 1, "sec");        
     setRegister(deviceHandle, RTC_MIN, 1, "min");        
     setRegister(deviceHandle, RTC_HOUR, 3, "hour");        
-
+*/
     // begin transmission and request acknowledgement
+
 	readBytes = write(deviceHandle, buffer, 1);
-	if (readBytes != 1)
-	{
+	if (readBytes != 1){
 		printf("Error: Received no ACK-Bit, couldn't established connection!");
 	}
-	else
-	{
+    else{
 		// read response
 		readBytes = read(deviceHandle, buffer, 7);
-		if (readBytes != 7)
-		{
+		if (readBytes != 7){
 			printf("Error: Received no data!");
 		}
-		else
-		{
+		else {
+            struct dt dateTime;
+
 			// get data
-			int seconds = buffer[0];	// 0-59
-			int minutes = buffer[1];	// 0-59
-			int hours = buffer[2];		// 1-23
-			int dayOfWeek = buffer[3];	// 1-7
-			int day = buffer[4];		// 1-28/29/30/31
-			int month = buffer[5];		// 1-12
-			int year = buffer[6];		// 0-99;
-			
-			// and print results
-			printf("Actual RTC-time:\n");
-			printf("Date: %x-%x-%x\n", year, month, day);
-			printf("Time: %x:%x:%x\n", hours, minutes, seconds);
+			dt.sec   = buffer[0]; // 0-59
+		    dt.min   = buffer[1]; // 0-59
+			dt.hour  = buffer[2]; // 1-23
+			dt.day   = buffer[3]; // 1-7
+			dt.mday  = buffer[4]; // 1-28/29/30/31
+			dt.month = buffer[5]; // 1-12
+			dt.year  = buffer[6]; // 0-99
+
 		}
 	}	
 
-	// close connection and return
 	close(deviceHandle);
 	return 0;
 }
