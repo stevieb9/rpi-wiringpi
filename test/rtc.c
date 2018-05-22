07ccaa0222c7172ccb41a9dc58106e9cb4f8244b
Steve Bertrand <steveb@cpan.org>
2018-05-21 17:38:26 -0700

    moved x.c to rtc.c with working code

--- test/rtc.c
+++ test/rtc.c
@@ -18,103 +18,76 @@
 #define RTC_AM_PM   0x20
 #define RTC_12_24   0x40
 
-uint8_t bcd2dec (uint8_t bcdByte)
+int bcd2dec (int bcdByte)
 {
   return (((bcdByte & 0xF0) >> 4) * 10) + (bcdByte & 0x0F);
 }
+
 int dec2bcd(int num){
    return((num/10) * 16 + (num%10));
 }
 
-int setRegister(int fd, int reg, uint8_t value, char* name){
+int establishI2C (int fd){
+    int buf[1] = { 0x00 };
+    if (write(fd, buf, 1) != 1){
+		printf("Error: Received no ACK-Bit, couldn't established connection!");
+        return -1;
+    }
+    return 0;
+}
+
+int getFh (){
+    int fd;
+
+    if ((fd = open("/dev/i2c-1", O_RDWR)) < 0) {
+        printf("Couldn't open the device: %s\n", strerror(errno));
+		return -1;
+	}
+
+	if (ioctl(fd, I2C_SLAVE_FORCE, RTC_ADDR) < 0) {
+        printf("Couldn't find device at addr %d: %s\n", RTC_ADDR, strerror(errno));
+		return -1;
+	}  
+
+    int established = establishI2C(fd);
+    return fd;
+}
+
+int setRegister(int fd, int reg, int value, char* name){
+
     char buf[2] = {reg, dec2bcd(value)};
     if ((write(fd, buf, sizeof(buf))) != 2){
         printf("Could not write the %s: %s\n", name, strerror(errno));
         return -1;
     }
+
     return 0;
 }
 
-int setDec(int fd, int reg, uint8_t value, char* name){
-    char buf[2] = {reg, value};
-    if ((write(fd, buf, sizeof(buf))) != 2){
-        printf("Could not write the %s: %s\n", name, strerror(errno));
-        return -1;
-    }
-    return 0;
+int getElement (int fd, int element){
+
+    char time[7];
+    time[0] = 0x00;
+    write(fd, time, 1);
+    read(fd, time, 7);
+
+    printf("%d:%d:%d\n",bcd2dec(time[2]),bcd2dec(time[1]),bcd2dec(time[0]));
+
+    return bcd2dec(time[element]);
 }
 
-int getRegister(int want, int dt[7]){
-    return dt[want];
-}
+int main (void){
 
-int main (void)
-{
-	int deviceHandle;
-	int readBytes;
-	char buffer[7];
- 
-	// initialize buffer
-	buffer[0] = 0x00;
-  
-	// open device on /dev/i2c-1
-	if ((deviceHandle = open("/dev/i2c-1", O_RDWR)) < 0) {
-        printf("Couldn't open the device: %s\n", strerror(errno));
-		return -1;
-	}
+    int fd = getFh();
 
-	// connect to DS1307 as i2c slave
-	if (ioctl(deviceHandle, I2C_SLAVE_FORCE, RTC_ADDR) < 0) {
-        printf("Couldn't find device at addr %d: %s\n", RTC_ADDR, strerror(errno));
-		return -1;
-	}  
- 
-    setRegister(deviceHandle, RTC_SEC, 10, "sec");        
-    setRegister(deviceHandle, RTC_MIN, 11, "min");        
-//    setRegister(deviceHandle, RTC_HOUR, 9, "hour");        
-    setRegister(deviceHandle, RTC_HOUR, 9 | 0b01000000, "12_24");        
-    printf("is 12: %d\n", 9 & 0b01000000);
-
-    // begin transmission and request acknowledgement
-
-	readBytes = write(deviceHandle, buffer, 1);
-	if (readBytes != 1){
-		printf("Error: Received no ACK-Bit, couldn't established connection!");
-	}
-    else{
-		// read response
-		readBytes = read(deviceHandle, buffer, 7);
-		if (readBytes != 7){
-			printf("Error: Received no data!");
-		}
-		else {
-
-            /*
-			// get data
-			dt.sec   = buffer[0]; // 0-59
-		    dt.min   = buffer[1]; // 0-59
-			dt.hour  = buffer[2]; // 1-23
-			dt.day   = buffer[3]; // 1-7
-			dt.mday  = buffer[4]; // 1-28/29/30/31
-			dt.month = buffer[5]; // 1-12
-			dt.year  = buffer[6]; // 0-99
-            */
-
-            int hour = bcd2dec(buffer[2]);
-            printf("hour: %d\n", hour);
-
-            if (hour >= 12){
-                hour = (hour - 12) | 0b01100000;
-            }
-            else {
-                hour = hour & 0b11011111;
-            }
-            printf("%d:%d:%d\n", hour, bcd2dec(buffer[1]), bcd2dec(buffer[0]));
-
-            printf("12/24: %d\n", hour & 0b10111111);
-		}
-	}	
-
-	close(deviceHandle);
+    printf("elem %d: %d\n", 0, getElement(fd, 0));
+    printf("elem %d: %d\n", 1, getElement(fd, 1));
+    printf("elem %d: %d\n", 2, getElement(fd, 2));
+    printf("elem %d: %d\n", 3, getElement(fd, 3));
+    printf("elem %d: %d\n", 4, getElement(fd, 4));
+    printf("elem %d: %d\n", 5, getElement(fd, 5));
+    printf("elem %d: %d\n", 6, getElement(fd, 6));
+    
+    close(fd);
 	return 0;
 }
