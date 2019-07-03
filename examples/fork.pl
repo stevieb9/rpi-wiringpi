f6bca106195a2a5281f25367bdd59ff0ad534dae
Steve Bertrand <steveb@cpan.org>
2019-07-03 14:36:11 -0700

    added fork test script temporarily

--- examples/fork.pl
+++ examples/fork.pl
@@ -7,34 +7,27 @@ use RPi::WiringPi;
 
 say "Process ID: $$";
 
-my $n = 1;
-my $forks = 0;
+my $pid = fork;
 
-for (1 .. $n) {
-    my $pid = fork;
+if (not defined $pid) {
+    warn 'Could not fork';
+    next;
+}
+if ($pid) {
+    $forks++;
+    say "in parent $$, child $pid";
+    my $pi = RPi::WiringPi->new(label => 'parent');
+    $pi->pin(12);
+    print Dumper $pi->metadata;
 
-    if (not defined $pid) {
-        warn 'Could not fork';
-        next;
-    }
-    if ($pid) {
-        $forks++;
-        say "in parent $$, child $pid";
-        my $pi = RPi::WiringPi->new(label => 'parent');
-        $pi->pin(12);
-        print Dumper $pi->metadata;
-
-        say "Parent obj... label: " . $pi->label . "\n";
-    } else {
-        my $pi = RPi::WiringPi->new(label => 'child');
-        sleep 2;
-        say "Child obj... label:" . $pi->label . "\n";;
-        exit;
-    }
+    say "Parent obj... label: " . $pi->label . "\n";
+} else {
+    my $pi = RPi::WiringPi->new(label => 'child');
+    sleep 2;
+    say "Child obj... label:" . $pi->label . "\n";;
+    exit;
 }
 
-for (1 .. $forks) {
-    my $pid = wait();
-#    say "Parent saw $pid exiting";
-}
-#say "Parent ($$) ending";
+my $pid = wait();
+say "Parent saw $pid exiting";
+say "Parent ($$) ending";
