34daf47714ad1818436ff9a99cba53daa82f188e
Steve Bertrand <steveb@cpan.org>
2019-07-05 12:47:30 -0700

    renumbered to 2.3633_01 TRIAL release

--- lib/RPi/WiringPi/Util.pm
+++ lib/RPi/WiringPi/Util.pm
@@ -10,7 +10,7 @@ use Digest::MD5 qw(md5_hex);
 use IPC::Shareable;
 use RPi::Const qw(:all);
 
-our $VERSION = '2.3633';
+our $VERSION = '2.3633_01';
 
 tie my %shared_pi_info, 'IPC::Shareable', {
     key => 'rpiw',
