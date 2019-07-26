81613b22cded98e667da65a32686e26a2043bd9c
Steve Bertrand <steveb@cpan.org>
2019-07-25 17:18:17 -0700

    re-enabled multi-die tests

--- t/multi/die_master.pl
+++ t/multi/die_master.pl
@@ -15,21 +15,26 @@ print "MASTER PROC: $$\n";
 my $f = 'ready.multi';
 
 my $pi = RPi::WiringPi->new(label => 'multi_die');
+my $meta;
 
 print "*** Single pin: Local & Remote ***\n\n";
 
-is exists($pi->metadata->{objects}{$pi->uuid}), 1, "$$ set in meta ok";
-is $pi->metadata->{objects}{$pi->uuid}{proc}, $$, "UUID proc set to procID $$ in meta ok";
-is keys %{ $pi->metadata->{objects} }, 2, "both procs have registered in meta";
+$meta = $pi->meta_fetch;
+
+is exists($meta->{objects}{$pi->uuid}), 1, "$$ set in meta ok";
+is $meta->{objects}{$pi->uuid}{proc}, $$, "UUID proc set to procID $$ in meta ok";
+is keys %{ $meta->{objects} }, 2, "both procs have registered in meta";
 
 $pi->pin(12, "112-die_master");
 
-is exists($pi->metadata->{pins}{12}), 1, "pin 12 exists for master proc ok";
-is $pi->metadata->{pins}{12}{users}{$pi->uuid}, 1, "pin 12 has local UUID as user ok";
-is exists($pi->metadata->{pins}{18}), 1, "pin 18 exists for slave ok";
-is $pi->metadata->{pins}{18}{users}{$pi->uuid}, undef, "pin 18 doesn't have local UUID as user ok";
+$meta = $pi->meta_fetch;
 
-is keys %{ $pi->metadata->{pins} }, 2, "three pins registered so far ok";
+is exists($meta->{pins}{12}), 1, "pin 12 exists for master proc ok";
+is $meta->{pins}{12}{users}{$pi->uuid}, 1, "pin 12 has local UUID as user ok";
+is exists($meta->{pins}{18}), 1, "pin 18 exists for slave ok";
+is $meta->{pins}{18}{users}{$pi->uuid}, undef, "pin 18 doesn't have local UUID as user ok";
+
+is keys %{ $meta->{pins} }, 2, "three pins registered so far ok";
 
 mywait();
 unlink $f or die $!;
@@ -38,22 +43,22 @@ sleep 1;
 
 print "*** External script: died() ***\n\n";
 
-$pi->dump_metadata;
+$meta = $pi->meta_fetch;
 
-is exists($pi->metadata->{objects}{$pi->uuid}), 1, "$$ set in meta ok";
-is $pi->metadata->{objects}{$pi->uuid}{proc}, $$, "UUID proc set to procID $$ in meta ok";
-is keys %{ $pi->metadata->{objects} }, 1, "back to one object";
+is exists($meta->{objects}{$pi->uuid}), 1, "$$ set in meta ok";
+is $meta->{objects}{$pi->uuid}{proc}, $$, "UUID proc set to procID $$ in meta ok";
+is keys %{ $meta->{objects} }, 1, "back to one object";
 
-is exists($pi->metadata->{pins}{12}), 1, "pin 12 exists for master proc ok";
-is $pi->metadata->{pins}{12}{users}{$pi->uuid}, 1, "pin 12 has local UUID as user ok";
-is exists($pi->metadata->{pins}{18}), '', "pin 18 no longer exists in slave";
+is exists($meta->{pins}{12}), 1, "pin 12 exists for master proc ok";
+is $meta->{pins}{12}{users}{$pi->uuid}, 1, "pin 12 has local UUID as user ok";
+is exists($meta->{pins}{18}), '', "pin 18 no longer exists in slave";
 
-is keys %{ $pi->metadata->{pins} }, 1, "one pin registered after slave die()";
+is keys %{ $meta->{pins} }, 1, "one pin registered after slave die()";
 
 $pi->cleanup;
 
 rpi_check_pin_status();
-rpi_metadata_clean();
+# rpi_metadata_clean();
 
 done_testing();
 
