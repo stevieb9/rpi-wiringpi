f2e5d28dd66c77a1da75e4eb06fa1ce0edb2de90
Steve Bertrand <steveb@cpan.org>
2019-09-03 15:06:19 -0700

    added hc12 basement monitor script and ino

--- build_testing/hc12/hc12-receive.pl
+++ build_testing/hc12/hc12-receive.pl
@@ -11,13 +11,14 @@ if (! $ARGV[0]){
     exit;
 }
 my $baud = $ARGV[0];
-my $dev = '/dev/ttyS0';
+my $dev = '/dev/ttyUSB0';
 
 my $s = RPi::Serial->new($dev, $baud);
 
 my $cmd;
 
 while ($run){
+    print $s->avail . "\n";
     if ($s->avail){
         my $char = $s->getc;
         $cmd .= chr $char;
